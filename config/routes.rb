6585b3d0a077f47eaf8cc069844f335bb4447a40
Steve_Drewery <11940@coderacademy.edu.au>
2022-11-08 19:07:06 +1000

    implented user authorization

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   scope '/api' do
+    resources :users, param: :username
     get '/workouts', to: 'workouts#index'
     post '/workouts', to: 'workouts#create'
     get '/workouts/:id', to: 'workouts#show'
     put '/workouts/:id', to: 'workouts#update'
     delete '/workouts/:id', to: 'workouts#destroy'
+    scope '/auth' do
+      post '/login', to: 'authentication#login'
+    end
   end
+  
 end
