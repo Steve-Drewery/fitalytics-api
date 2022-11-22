ba70b9886329cd20c68b78fd8e3d744a9f805e70
Steve_Drewery <11940@coderacademy.edu.au>
2022-11-22 19:46:37 +1000

    fixed weekly goal min

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
     has_many :workouts, dependent: :destroy
     validates :email, presence: true, uniqueness: true
     validates :username, presence: true, uniqueness: true
-    validates :weekly_goal, numericality: { greater_than: 0, less_than_or_equal_to: 7 }
+    validates :weekly_goal, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 7 }
 end
